7b4c8b78ebe3a9c9608e0e32f4ae031d2f1a6b4f
TomiTsuma <34827918+TomiTsuma@users.noreply.github.com>
2020-06-04 13:12:32 -0700

    Update ThomasTsuma.py

--- ThomasTsuma.py
+++ ThomasTsuma.py
@@ -2,4 +2,4 @@
 ID = "HNG-00332"
 email = "tommytsuma7@gmail.com"
 language = "Python"
-print("Hello World, this is ".fullName." with HNGi7 ID ".[ID]." and email ".[email]." using ."[language]." for stage 2 task")
+print("Hello World, this is {fullName} with HNGi7 ID {ID} and email {email} using {language} for stage 2 task")
