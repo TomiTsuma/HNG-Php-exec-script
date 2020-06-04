700049b6bd91413ba770489acef6326a4b379b56
TomiTsuma <34827918+TomiTsuma@users.noreply.github.com>
2020-06-04 12:37:50 -0700

    Add files via upload

---
+++ ThomasTsuma.py
@@ -0,0 +1,5 @@
+﻿fullName = "Thomas Tsuma"
+ID = "HNG-00332"
+email = "tommytsuma7@gmail.com"
+language = "Python"
+print("Hello World, this is ".fullName." with HNGi7 ID ".[ID]." and email ".[email]." using ."[language]." for stage 2 task")
